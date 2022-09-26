24d6bbe680e226cb20b84e0be595d7e19e53944c
Synnomar <synnomar@hotmail.com>
2022-09-26 12:54:14 -0300

    atualizar aulas de python

---
+++ Python Para Ciência de Dados/AmbDesenvPrimPasso/TiposDados.py
@@ -0,0 +1,9 @@
+print(11 + 10 + 1000)
+print(1.5 + 1 + 0.5)
+print(True)
+print("Python")
+
+int()
+float()
+str()
+bool()
---
+++ Python Para Ciência de Dados/AmbDesenvPrimPasso/variavel.py
@@ -0,0 +1,6 @@
+age = 38
+name = 'Sinomar Melo'
+print(f'Meu nome é {name} e eu tenho {age} anos de idade. ')
+
+age, name = (38, 'Sinomar Melo')
+print(f'Meu nome é {name} e eu tenho {age} anos de idade. ')
