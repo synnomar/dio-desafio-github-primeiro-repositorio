81b467dee4e59cdb203e12229b253450639961d2
Synnomar <synnomar@hotmail.com>
2022-09-28 16:46:38 -0300

    atualizar aulas de python operadores de associacao

--- Python Para Ciência de Dados/AmbDesenvPrimPasso/10operador_logico.py
+++ Python Para Ciência de Dados/AmbDesenvPrimPasso/10operador_logico.py
@@ -1,6 +1,23 @@
+print(True and True and True)
+print(True and False and True)
+print(False and True and True)
+print(True and True and True)
+print(True and True or False)
+print(True or True and False)
+
 saldo = 1000
 saque = 200
 limite = 100
+conta_especial = True
 
-print(saldo >= saque and saque <= limite)
+exp = saldo >= saque and saque <= limite or conta_especial and saldo >= saque
+print(exp)
 
+exp_2 = (saldo >= saque and saque <= limite) or (conta_especial and saldo >= saque)
+print(exp_2)
+
+conta_normal_com_saldo_suficiente = saldo >= saque and saque <= limite 
+conta_especial_com_saldo_suficiente = conta_especial and saldo >= saque
+
+exp_3 = conta_normal_com_saldo_suficiente or conta_especial_com_saldo_suficiente
+print(exp_3)    
